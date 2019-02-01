764eaefc1a1ccddda2c6e681550219827338d91c
Glory Praise <emmaglorypraise@gmail.com>
2019-02-01 14:55:50 +0100

    all files for my portfolio website...updates later
    my personal portfolio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// ------- PRELOADER -------//
+$(window).load(function(){
+    $('.preloader').fadeOut("slow"); // set duration in brackets    
+});
+
+
+// ------- WOW ANIMATED ------ //
+  wow = new WOW(
+    {
+      mobile: false
+    });
+    wow.init();
+  
